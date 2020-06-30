03865f993ccab123f667274baa4c30e365899fa6
Ivan <39524682+Skrals@users.noreply.github.com>
2020-06-30 17:48:11 +0300

    Оружие

--- Assets/Scripts/Player scripts/PlayerControl.cs
+++ Assets/Scripts/Player scripts/PlayerControl.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    CharacterController player;
+
     void Start()
     {
+        float x;
+        float y;
+        float z;
         
+
     }
+    
 
     // Update is called once per frame
-    void Update()
-    {
+    void FixUpdate()
+    { 
+        
         
     }
 }
