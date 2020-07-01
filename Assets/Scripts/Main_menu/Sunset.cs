8f7b5dd19e9a8cb9cda9dcbc086cbf62ecee31c4
Ivan <39524682+Skrals@users.noreply.github.com>
2020-07-01 18:22:37 +0300

    Menu_up

---
+++ Assets/Scripts/Main_menu/Sunset.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sunset : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    private float r = 0.5f;
+    private float g = 0.5f;
+    private float b = 0.5f;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rotation = transform.rotation.eulerAngles;
+        rotation.x -= Time.deltaTime/2;
+        transform.rotation = Quaternion.Euler(rotation);
+
+        this.GetComponent<Light>().color = new Color(r, g, b);
+        r += Time.deltaTime/100;
+        g -= Time.deltaTime / 100;
+    }
+}
