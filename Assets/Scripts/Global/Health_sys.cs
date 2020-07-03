91139139040854f09f7d1e1552b0df396e9b1b0f
Ivan <39524682+Skrals@users.noreply.github.com>
2020-07-03 05:56:12 +0300

    Система попаданий

---
+++ Assets/Scripts/Global/Health_sys.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health_sys : MonoBehaviour
+{
+    public float head = 100f;
+    public float chest = 100f;
+    public float right_arm = 100f;
+    public float left_arm = 100f;
+    public float right_leg = 100f;
+    public float left_leg = 100f;
+
+    public void Params(float h, float c, float r_a, float l_a, float r_l, float l_l)
+    {
+        head = h;
+        chest = c;
+        right_arm = r_a;
+        left_arm = l_a;
+        right_leg = r_l;
+        left_leg = l_l;
+    }
+
+}
---
+++ Assets/Scripts/Player scripts/Weapon/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Enemy enemy;
+    void Start()
+    {
+        enemy = gameObject.AddComponent<Enemy>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        EnemyHead head = collision.collider.GetComponent<EnemyHead>();
+        if(head)
+        {
+            enemy.HitHead(50);
+        }
+        Destroy(gameObject);
+    }
+}
