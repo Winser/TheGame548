b6003a646bf8dffb8b7c85bbf54aaa1c9fa7d100
Ivan <39524682+Skrals@users.noreply.github.com>
2020-07-04 23:47:47 +0300

    Update BulletCreator.cs

--- Assets/Scripts/Player scripts/Weapon/BulletCreator.cs
+++ Assets/Scripts/Player scripts/Weapon/BulletCreator.cs
@@ -7,17 +7,37 @@ public class BulletCreator : MonoBehaviour
 {
     public GameObject BulletPrefab;
     public float BulletVelocity = 20f;
-
+    public bool is_automatic = false;
     void Update()
     {
-
         if (Input.GetMouseButtonDown(0))
+        {
+            if (!is_automatic)
+            {
+
+                Shoot();
+            }
+            else
+            { 
+                Rifle_shoot();
+            }
+        }
+    }
+    void Shoot ()
+    {
+        GameObject Bullet = Instantiate(BulletPrefab, transform.position, transform.rotation);
+        Bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * BulletVelocity;
+        this.GetComponent<ParticleSystem>().Play();
+        this.GetComponent<AudioSource>().Play();
+    }
+    void Rifle_shoot()
+    {
+        for (float i = 0; i < 3; i++)
         {
             GameObject Bullet = Instantiate(BulletPrefab, transform.position, transform.rotation);
             Bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * BulletVelocity;
             this.GetComponent<ParticleSystem>().Play();
             this.GetComponent<AudioSource>().Play();
-
         }
     }
 }
