a3084f9346b586d0da8f66a042606c367c68d128
Ivan <39524682+Skrals@users.noreply.github.com>
2020-07-04 18:56:01 +0300

    Подсветка

--- Assets/Scripts/Global/highlighting.cs
+++ Assets/Scripts/Global/highlighting.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class highlighting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    private void OnMouseEnter()
     {
-        
+        if(tag == "Stuff" || tag == "Wolf")
+        {
+            GetComponent<Light>().enabled = true;
+        }
+    }
+    private void OnMouseExit()
+    {
+        if (tag == "Stuff" || tag == "Wolf")
+        {
+            GetComponent<Light>().enabled = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
