d1eae022a73b271e094ab5df7907d54344bd445f
Ivan <39524682+Skrals@users.noreply.github.com>
2020-07-06 01:54:48 +0300

    Префабы
    Обновление скриптов с воспроизведением звука и префабы Bullet\Shell creator

--- Assets/Scripts/Player scripts/Weapon/BulletCreator.cs
+++ Assets/Scripts/Player scripts/Weapon/BulletCreator.cs
@@ -6,63 +6,88 @@ using UnityEngine;
 public class BulletCreator : MonoBehaviour
 {
     public GameObject BulletPrefab;
+    public GameObject Shell;
     public float BulletVelocity = 20f;
     public bool is_automatic = false;
     public bool IsAiming = false;
+    public int Ammo = 25;
     //cool donw timer
     public float CD;
     void Update()
     {
         CoolDown();
-        if (Input.GetKeyDown("1"))
+        Reload();
+        if (CD > 3)
         {
-            if (IsAiming)
+            if (Input.GetKeyDown("1"))
             {
-                IsAiming = false;
-            }
-            else
-            {
-                IsAiming = true;
-            }
-        }
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (IsAiming)
-            {
-                if (!is_automatic)
+                if (IsAiming)
                 {
-                    Shoot();
+                    IsAiming = false;
                 }
                 else
                 {
-                   // Rifle_shoot();
+                    IsAiming = true;
+                }
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (IsAiming)
+                {
+                    if (!is_automatic)
+                    {
+                        Shoot();
+                    }
+                    else
+                    {
+                        // Rifle_shoot();
+                    }
                 }
             }
         }
     }
-    void Shoot ()
+    void Reload()
     {
-        
+        if (Input.GetKeyDown("r"))
+        {
+            Ammo = 25;
+            Shell.gameObject.SetActive(true);
+            GameObject.Find("Reload_sound").GetComponent<AudioSource>().Play();
+            CD = 0;
+        }
+    }
+    void Shoot()
+    {
+        if (Ammo > 0)
+        {
             GameObject Bullet = Instantiate(BulletPrefab, transform.position, transform.rotation);
             Bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * BulletVelocity;
             this.GetComponent<ParticleSystem>().Play();
             this.GetComponent<AudioSource>().Play();
             CD = 0f;
             IsAiming = false;
-        
+            Ammo -= 1;
+        }
+        else
+        {
+
+            Shell.gameObject.SetActive(false);
+            GameObject.Find("Empty_sound").GetComponent<AudioSource>().Play();
+            IsAiming = false;
+        }
 
     }
     private void CoolDown()
     {
         CD += Time.deltaTime;
     }
-    
-   /* void Rifle_shoot()
-    {
-        
-            GameObject Bullet = Instantiate(BulletPrefab, transform.position, transform.rotation);
-            Bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * BulletVelocity;
-            this.GetComponent<ParticleSystem>().Play();
-            this.GetComponent<AudioSource>().Play();
-    }*/
+
+    /* void Rifle_shoot()
+     {
+
+             GameObject Bullet = Instantiate(BulletPrefab, transform.position, transform.rotation);
+             Bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * BulletVelocity;
+             this.GetComponent<ParticleSystem>().Play();
+             this.GetComponent<AudioSource>().Play();
+     }*/
 }
