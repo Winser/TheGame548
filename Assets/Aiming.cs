b55c123c85f9f834ea1eba10dcbd25ab1232e0bb
Ivan <39524682+Skrals@users.noreply.github.com>
2020-07-06 20:23:22 +0300

    /

---
+++ Assets/Aiming.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aiming : MonoBehaviour
+{
+   
+    public Transform weapon;
+    private float ray_lenght = 100f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(weapon.position, weapon.forward * ray_lenght,Color.red);
+        Ray ray = new Ray(weapon.position, weapon.forward * ray_lenght);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit));
+        Rotation();
+
+    }
+
+    private void Rotation ()
+    {
+       
+
+        
+    }
+  
+}
