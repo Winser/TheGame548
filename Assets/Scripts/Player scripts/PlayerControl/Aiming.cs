9b3bf6186f33d816d7670120fa82d8e1a8e9767d
Ivan <39524682+Skrals@users.noreply.github.com>
2020-07-06 21:04:43 +0300

    прицеливание
    не работает

--- Assets/Scripts/Player scripts/PlayerControl/Aiming.cs
+++ Assets/Scripts/Player scripts/PlayerControl/Aiming.cs
@@ -7,6 +7,9 @@ public class Aiming : MonoBehaviour
    
     public Transform weapon;
     private float ray_lenght = 100f;
+    private float X;
+    private float Y;
+    private Vector3 mouse;
 
     void Start()
     {
@@ -21,13 +24,16 @@ public class Aiming : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit));
         Rotation();
-
+        
     }
 
     private void Rotation ()
     {
-       
+        mouse =  Input.mousePosition;
+        X = mouse.x;
+        Y = mouse.y;
 
+        weapon.rotation = Quaternion.Euler(X, Y, 0);
         
     }
   
