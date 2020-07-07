738dee99ac80d7b5215e76d748c021a75e02b820
Ivan <39524682+Skrals@users.noreply.github.com>
2020-07-07 11:39:57 +0300

    Привязал отображение пуль

--- Assets/Scripts/Player scripts/Inventory/UI_main.cs
+++ Assets/Scripts/Player scripts/Inventory/UI_main.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_main : MonoBehaviour
 {
-    
+    private int MaxBullets;
+    private int CurrBullets;
+    private BulletCreator BulletSource;
+    void Start()
+    {
+       BulletSource = GameObject.Find("Bullet_creator").GetComponent<BulletCreator>();
+        MaxBullets = BulletSource.MaxAmmo;
+    }
     void Update()
     {
-        
+        CurrBullets = BulletSource.Ammo;
+        GameObject.Find("BulletsKolVo").GetComponent<Text>().text = CurrBullets + " / " + MaxBullets;
     }
 }
