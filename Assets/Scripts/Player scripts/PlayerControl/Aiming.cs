092ec80161be5917e14ecee66423467c9ce4ee45
Ivan <39524682+Skrals@users.noreply.github.com>
2020-07-07 06:42:54 +0300

    AimingRay

--- Assets/Scripts/Player scripts/PlayerControl/Aiming.cs
+++ Assets/Scripts/Player scripts/PlayerControl/Aiming.cs
@@ -7,18 +7,18 @@ public class Aiming : MonoBehaviour
     public Camera cam;
     private float x;
     public Transform weapon;
-    private Vector3 VorlPos;
     private float ray_lenght = 100f;
     public float mouseSense = 5f;
     public bool isAiming = false;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         isAiming = MovableCharacter.IsAiming;
         Rotation();
     }
@@ -28,21 +28,22 @@ public class Aiming : MonoBehaviour
         if (isAiming)
         {
             Debug.DrawRay(weapon.position, weapon.forward * ray_lenght, Color.red);
-           VorlPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            if (Physics.Raycast(ray,out hit))
+            if (Physics.Raycast(ray, out hit))
             {
                 x = hit.point.x;
                 weapon.LookAt(hit.point);
+                Vector3 bullet_creator_pos = GameObject.Find("Bullet_creator").GetComponent<Transform>().position;
+                GameObject.Find("Bullet_creator").GetComponent<LineRenderer>().SetPosition(0, bullet_creator_pos); ;
+                GameObject.Find("Bullet_creator").GetComponent<LineRenderer>().SetPosition(1, hit.point);
             }
-
-            //weapon.Rotate(Vector3.right, Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSense);
 
         }
         else
         {
-            
+            GameObject.Find("Bullet_creator").GetComponent<LineRenderer>().SetPosition(0, Vector3.zero);
+            GameObject.Find("Bullet_creator").GetComponent<LineRenderer>().SetPosition(1, Vector3.zero);
         }
     }
 
