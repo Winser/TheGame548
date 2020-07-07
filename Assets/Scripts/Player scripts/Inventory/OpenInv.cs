a81afaf8e72a0004fd721e13758ddd0edd0b3f6d
Slashman <By4uykin@yandex.ru>
2020-07-07 10:09:36 +0300

    Открытие при нажатии на рюкзак(или Q)

---
+++ Assets/Scripts/Player scripts/Inventory/OpenInv.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenInv : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject MyInventoryAll;
+    public void OpenMyInv()
+    {
+        MyInventoryAll.SetActive(true);
+    }
+    public void CloseMyInv()
+    {
+        MyInventoryAll.SetActive(false);
+    }
+}
