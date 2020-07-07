4050f3e4523e1c51bb53b356fed9ad5fb08e0a9f
Slashman <By4uykin@yandex.ru>
2020-07-07 09:20:49 +0300

    Скрытие инвентаря кнопка Q

--- Assets/Scripts/Player scripts/Inventory.cs
+++ Assets/Scripts/Player scripts/Inventory.cs
@@ -5,15 +5,36 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
     
-    List<Item> item; 
+    List<Item> item;
+    public GameObject cellContainer;
+    public GameObject InventoryAll;
+    public KeyCode ShowInventory;
     void Start()
     {
         item = new List<Item>();
+
+        InventoryAll.SetActive(false);
+
+        for (int i = 0; i < cellContainer.transform.childCount; i++)
+        {
+            item.Add(new Item());
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(ShowInventory))
+        {
+            if (InventoryAll.activeSelf)
+            {
+                InventoryAll.SetActive(false);
+            }
+            else
+            {
+                InventoryAll.SetActive(true);
+            }
+        }
     }
 }
