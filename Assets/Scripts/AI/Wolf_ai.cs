f63269059200d9a89245ac797c299a8a33a5b0b5
Ivan <39524682+Skrals@users.noreply.github.com>
2020-07-08 08:31:34 +0300

    Update Wolf_ai.cs

--- Assets/Scripts/AI/Wolf_ai.cs
+++ Assets/Scripts/AI/Wolf_ai.cs
@@ -7,9 +7,13 @@ public class Wolf_ai : MonoBehaviour
 
     private GameObject player_pos;
     public float dist;
+    //bool random = false;
+   // public float random_dist;
     NavMeshAgent nav;
     private Animator animator;
     public float visibility_radius;
+    //private float random_poin;
+   // private float timer;
     void Start()
     {
         visibility_radius = 20f;
@@ -21,13 +25,17 @@ public class Wolf_ai : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       // timer += Time.deltaTime;
+        
         dist = Vector3.Distance(player_pos.transform.position, transform.position);
+        //MoveToRandomPoint();
         if(dist > visibility_radius)
         {
             nav.enabled = false;
             animator.SetBool("idle01", true);
             animator.SetBool("attack", false);
             animator.SetBool("run", false);
+            //random = true;
         }
         if (dist < visibility_radius && dist> 3f)
         {
@@ -36,6 +44,7 @@ public class Wolf_ai : MonoBehaviour
             nav.enabled = true;
             nav.SetDestination(player_pos.transform.position);
             animator.SetBool("run",true);
+          //  random = false;
 
         }
         if(dist <= 3f)
@@ -49,4 +58,21 @@ public class Wolf_ai : MonoBehaviour
             visibility_radius = 0f;
         }
     }
+
+   /* void MoveToRandomPoint ()
+    {
+        if (timer > 5)
+        {
+            random_poin = Random.Range(15, 250);
+            random_dist = Vector3.Distance(new Vector3(random_poin, random_poin), transform.position);
+            timer = 0f;
+        }
+        if (random)
+        {
+            nav.enabled = true;
+           
+            nav.SetDestination((new Vector3(random_poin, random_poin)));
+        }
+    }
+    */
 }
